df9f90a2e2579a6ef34b80f2291b97d4a267ef44
Dave Madden <davelearnsstuff@gmail.com>
2021-09-24 12:46:15 -0700

    isPrime working and passes.

--- is-prime/isprime.js
+++ is-prime/isprime.js
@@ -1,12 +1,16 @@
 console.log("testing works");
 
 function isPrime(num) {
-    if ((num <= 1) || !num%2 || !num%3 || !num%5){ //greater than zero, not divisible by 2, 3, or 5
-        return false;
+    if ((num <= 1) || (!(num % 2) && num !=2) || (!(num % 3) && num != 3) || (!(num % 5) && num != 5)){ //greater than zero, not divisible by 2, 3, or 5
+        return false; // || (Math.sqrt(num) === Math.floor(Math.sqrt(num)) 
     }
-    for(let i=0; i<Math.sqrt(num); i++){
-        console.log("in the for loop")
+    for(let i=3; i <= Math.sqrt(num); i=i+2){
+        n = num / i;
+        if (n === Math.floor(n)){
+            return false;
+        }
     }
+    return true;
   }
 
 //tests
@@ -21,3 +25,5 @@ testPrime(-1)
 testPrime(0)
 testPrime(10)
 testPrime(73)
+testPrime(81)
+testPrime(200194201) //factors = 14149
