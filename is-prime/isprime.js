00a8ab427463a89be14d5f635b6dcfc2257dbfe5
Dave Madden <davelearnsstuff@gmail.com>
2021-09-24 12:18:14 -0700

    isprime scaffolding up

--- is-prime/isprime.js
+++ is-prime/isprime.js
@@ -1,8 +1,23 @@
 console.log("testing works");
 
 function isPrime(num) {
-    if (num <= 1){
+    if ((num <= 1) || !num%2 || !num%3 || !num%5){ //greater than zero, not divisible by 2, 3, or 5
         return false;
     }
-    
+    for(let i=0; i<Math.sqrt(num); i++){
+        console.log("in the for loop")
+    }
   }
+
+//tests
+function testPrime(num){
+    console.log(`is ${num} prime? ${isPrime(num)}`);
+}
+
+testPrime(1)
+testPrime(2)
+testPrime(3)
+testPrime(-1)
+testPrime(0)
+testPrime(10)
+testPrime(73)
