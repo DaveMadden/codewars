24fbaff42349f6be0cc1fe7ef87f586adc9e1500
Dave Madden <davelearnsstuff@gmail.com>
2021-09-24 11:15:18 -0700

    set up

---
+++ calc-functions/calcfunc.js
@@ -0,0 +1,17 @@
+// console.log("testing works");
+
+function zero() {}
+function one() {}
+function two() {}
+function three() {}
+function four() {}
+function five() {}
+function six() {}
+function seven() {}
+function eight() {}
+function nine() {}
+
+function plus() {}
+function minus() {}
+function times() {}
+function dividedBy() {}
